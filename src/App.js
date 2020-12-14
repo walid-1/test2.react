664a20e5c3b445378f1ad08c335bea56e6bd9e53
walid <wilwil1515@hotmail.com>
2020-12-14 15:43:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import imageInSrc from './port.jpg';
+import videoSrc from './videoSrc.mp4'
 import './App.css';
 
 function App() {
@@ -7,13 +8,12 @@ function App() {
     <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
       <h1 className="title red">react app</h1>
       <br />
-      <img src="/telechargement.jpg" />
+      <img src='/téléchargement.jpg' alt='a' />
       <br />
-      <img src="/port.jpg" />
+      <img src={imageInSrc} alt='b' />
     </div>
-    <video width={320} height={240} controls>
-          <source src="myVideo.mp4" type="video/mp4" />
-        </video>
+    <video width={320} height={240} controls src= {videoSrc} />
+         
   </div>
   );
 }
